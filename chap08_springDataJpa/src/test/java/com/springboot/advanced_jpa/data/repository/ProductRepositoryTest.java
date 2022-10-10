dbb287ca3810f3d23b16804df42db26084b0701f
steptoreal <jcob0920@gmail.com>
2022-10-10 17:15:45 +0900

    chap08 - update
    QueryDsl 의존성 및 플러그인 추가
    QueryDsl Test 코드 작성

--- chap08_springDataJpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
+++ chap08_springDataJpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -1,24 +1,34 @@
 package com.springboot.advanced_jpa.data.repository;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.advanced_jpa.data.entity.Product;
+import com.springboot.advanced_jpa.data.entity.QProduct;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
 
     @Test
+    @org.junit.jupiter.api.Order(1)
     void sortingAndPagingTest() {
         Product product1 = new Product();
         product1.setName("펜");
@@ -55,5 +65,89 @@ public class ProductRepositoryTest {
 
     private Sort getSort() {
         return Sort.by(Order.asc("price"), Order.asc("stock"));
+    }
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Test
+    @org.junit.jupiter.api.Order(2)
+    void queryDslTest() {
+        JPAQuery<Product> query = new JPAQuery(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = query
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Product product : productList) {
+            System.out.println("-------------------------");
+            System.out.println();
+            System.out.println("Product Number : " + product.getNumber());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Product Price : " + product.getPrice());
+            System.out.println("Product Stock : " + product.getStock());
+            System.out.println();
+            System.out.println("-------------------------");
+        }
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(3)
+    void queryDslTest2() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = jpaQueryFactory.selectFrom(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Product product : productList) {
+            System.out.println("-------------------------");
+            System.out.println();
+            System.out.println("Product Number : " + product.getNumber());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Product Price : " + product.getPrice());
+            System.out.println("Product Stock : " + product.getStock());
+            System.out.println();
+            System.out.println("-------------------------");
+        }
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(4)
+    void queryDslTest3() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<String> productList = jpaQueryFactory
+                .select(qProduct.name)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (String product : productList) {
+            System.out.println("-----------------------");
+            System.out.println("Product Name : " + product);
+            System.out.println("-----------------------");
+        }
+
+        List<Tuple> tupleList = jpaQueryFactory
+                .select(qProduct.name, qProduct.price)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Tuple product : tupleList) {
+            System.out.println("----------------------");
+            System.out.println("Product Name : " + product.get(qProduct.name));
+            System.out.println("Product Name : " + product.get(qProduct.price));
+            System.out.println("----------------------");
+        }
     }
 }
