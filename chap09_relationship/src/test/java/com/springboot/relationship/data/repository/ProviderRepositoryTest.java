dc375a358b95b1a787f79d5716ab8acda32e19f2
steptoreal <jcob0920@gmail.com>
2022-10-15 00:26:28 +0900

    chap09 upload
    다대일 양방향 매핑 예제 구현

--- chap09_relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
+++ chap09_relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class ProviderRepositoryTest {
 
@@ -31,8 +33,53 @@ public class ProviderRepositoryTest {
 
         productRepository.save(product);
 
+        Product product2 = new Product();
+        product2.setName("종이");
+        product2.setPrice(1000);
+        product2.setStock(100);
+        product2.setProvider(provider);
+
+        productRepository.save(product2);
+
         //테스트
         System.out.println("product : " + productRepository.findById(1L).orElseThrow(RuntimeException::new));
         System.out.println("provider : " + productRepository.findById(1L).orElseThrow(RuntimeException::new).getProvider());
+    }
+
+    @Test
+    void relationshipTest() {
+        //테스트 데이터 생성
+        Provider provider = new Provider();
+        provider.setName("ㅇㅇ상사");
+
+        providerRepository.save(provider);
+
+        Product product1 = new Product();
+        product1.setName("펜");
+        product1.setPrice(2000);
+        product1.setStock(100);
+        product1.setProvider(provider);
+
+        Product product2 = new Product();
+        product2.setName("가방");
+        product2.setPrice(20000);
+        product2.setStock(200);
+        product2.setProvider(provider);
+
+        Product product3 = new Product();
+        product3.setName("노트");
+        product3.setPrice(3000);
+        product3.setStock(1000);
+        product3.setProvider(provider);
+
+        productRepository.save(product1);
+        productRepository.save(product2);
+        productRepository.save(product3);
+
+        List<Product> products = providerRepository.findById(provider.getId()).get().getProductList();
+
+        for (Product product : products) {
+            System.out.println(product);
+        }
     }
 }
