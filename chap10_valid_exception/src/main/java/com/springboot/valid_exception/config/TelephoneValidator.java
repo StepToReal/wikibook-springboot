0de1a3a76963cd5c30237a377921091428b1b69a
steptoreal <jcob0920@gmail.com>
2022-10-20 23:04:31 +0900

    chap10 - custom validation 실습 예제 구현

---
+++ chap10_valid_exception/src/main/java/com/springboot/valid_exception/config/TelephoneValidator.java
@@ -0,0 +1,18 @@
+package com.springboot.valid_exception.config;
+
+import com.springboot.valid_exception.config.annotation.Telephone;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TelephoneValidator implements ConstraintValidator<Telephone, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+
+        return value.matches("01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$");
+    }
+}
